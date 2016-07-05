c8e07d3c792a5a9f9dcd9783540df1a63b269c4d
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-07-05 23:22:59 +0200

    update install.sh: Remove hard-coded version and suppress error messages

--- install.sh
+++ install.sh
@@ -11,14 +11,16 @@ echo "#1. compiling the library from source code and dependencies"
 mvn clean
 mvn install
 echo "#2. moving binary to lib folder"
-mv "$DIR/target/EventCoreference-v3.0-jar-with-dependencies.jar" "$DIR/lib"
+mv ${DIR}/target/EventCoreference-*-jar-with-dependencies.jar "$DIR/lib"
 echo "#3. installing the vua-resources"
 cd "$PARENT"
-git clone https://github.com/cltl/vua-resources.git
+if [ ! -d vua-resources ]; then 
+    git clone https://github.com/cltl/vua-resources.git
+fi
 echo "#4. cleaning up"
 cd "$DIR"
-rm -R target
-rm -R src
-rm EventCoreference.iml
-rm EventCoreference.iws
-rm EventCoreference.ipr
+rm -Rf target
+rm -Rf src
+rm -f EventCoreference.iml
+rm -f EventCoreference.iws
+rm -f EventCoreference.ipr
