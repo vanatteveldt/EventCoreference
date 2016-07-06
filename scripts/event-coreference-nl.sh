b9155b4066ece5db4f1faf8e656620c2586ebc6c
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-07-06 22:01:17 +0200

    Get version from pom.xml (fixes #5)

--- scripts/event-coreference-nl.sh
+++ scripts/event-coreference-nl.sh
@@ -1,7 +1,9 @@
+set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT="$( cd $DIR && cd .. && pwd)"
 LIB="$ROOT"/lib
 REL="XPOS_NEAR_SYNONYM#HAS_HYPERONYM#HAS_XPOS_HYPERONYM#event"
+VERSION=`python $DIR/pom_version.py`
 
 RESOURCES="$( cd $ROOT && cd .. && pwd)"/vua-resources
 # assumes vua-resources is installed next to this installation
@@ -10,4 +12,4 @@ RESOURCES="$( cd $ROOT && cd .. && pwd)"/vua-resources
 #pass naf file as input stream and catch the naf output stream
 # for example> "cat example-naf.xml | event-coreference.sh > example-naf.coref.xml"
 
-java -Xmx812m -cp "$LIB/EventCoreference-v3.0-jar-with-dependencies.jar" eu.newsreader.eventcoreference.naf.EventCorefWordnetSim --method leacock-chodorow --wn-lmf "$RESOURCES/odwn_orbn_gwg-LMF_1.3.xml.gz" --sim 2.0 --sim-ont 0.6 --wsd 0.8 --relations $REL --source-frames "$RESOURCES/source.txt" --grammatical-frames "$RESOURCES/grammatical.txt" --contextual-frames "$RESOURCES/contextual.txt"
+java -Xmx812m -cp "$LIB/EventCoreference-$VERSION-jar-with-dependencies.jar" eu.newsreader.eventcoreference.naf.EventCorefWordnetSim --method leacock-chodorow --wn-lmf "$RESOURCES/odwn_orbn_gwg-LMF_1.3.xml.gz" --sim 2.0 --sim-ont 0.6 --wsd 0.8 --relations $REL --source-frames "$RESOURCES/source.txt" --grammatical-frames "$RESOURCES/grammatical.txt" --contextual-frames "$RESOURCES/contextual.txt"
